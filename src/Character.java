ad3901df92ce31271283bf21ba34a4bc466a5c25
Andrew <andrewmmless@gmail.com>
2026-08-24 16:26:30 -0500

    Created Character.java for character information

---
+++ src/Character.java
@@ -0,0 +1,12 @@
+public class Character {
+    private int health;
+    private String name;
+    private String className;
+
+    public Character(int health, String name) {
+        this.health = health;
+        this.name = name;
+        this.className = name;
+    }
+}
+
